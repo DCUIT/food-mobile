5c57fd6cb4741597deba83155a8ea0f6f5dca2d1
DCUIT <20521120@gm.uit.edu.vn>
2026-05-05 14:59:43 +0700

    update THEME / COLORS

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Primary
+  static const Color primary = Color(0xFFFF6B35);
+  static const Color primaryDark = Color(0xFFE55A26);
+  
+  // Secondary
+  static const Color secondary = Color(0xFF4ECDC4);
+  static const Color accentGreen = Color(0xFF45B7D1);
+  
+  // Neutral
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF1A1A1A);
+  static const Color lightGrey = Color(0xFFF8F9FA);
+  static const Color mediumGrey = Color(0xFFDEE2E6);
+  static const Color darkGrey = Color(0xFF6C757D);
+  static const Color textGrey = Color(0xFF495057);
+  
+  // Success/Error
+  static const Color success = Color(0xFF28A745);
+  static const Color warning = Color(0xFFFFC107);
+  static const Color error = Color(0xFFDC3545);
+  
+  // Backgrounds
+  static const Color backgroundLight = Color(0xFFFFFBF8);
+  static const Color surfaceLight = Color(0xFFFFFFFF);
+  
+  static const Color backgroundDark = Color(0xFF121212);
+  static const Color surfaceDark = Color(0xFF1E1E1E);
+}
