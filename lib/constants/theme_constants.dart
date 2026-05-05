f9d54b670fdb29ac4ffc2b6e25354bdfc64991e5
DCUIT <20521120@gm.uit.edu.vn>
2026-05-05 14:55:50 +0700

    update LOADING and ERROR STATE

--- lib/constants/theme_constants.dart
+++ lib/constants/theme_constants.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 const Color primaryColor = Colors.orange;
 const Color accentColor = Colors.green;
 const Color textColor = Colors.grey;
+const double kPadding = 16.0;
+const double kCardElevation = 4.0;
+const double kBorderRadius = 12.0;
 
 ThemeData appTheme() {
   return ThemeData(
@@ -15,3 +18,10 @@ ThemeData appTheme() {
     ),
   );
 }
+
+class Responsive {
+  static bool isMobile(BuildContext context) => MediaQuery.of(context).size.width < 600;
+  static bool isTablet(BuildContext context) => MediaQuery.of(context).size.width >= 600 && MediaQuery.of(context).size.width < 1200;
+  static double gridChildAspectRatio(BuildContext context) => Responsive.isTablet(context) ? 1.2 : 0.75;
+}
+
